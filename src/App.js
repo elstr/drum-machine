57cda0068841a7453d89aa4444cb1d4f0e78d613
elstr <eleonora.lester@gmail.com>
2020-02-03 17:49:54 -0300

    feat: Render drum machine components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Components
+import Controls from "./components/Controls";
+import Channel from "./components/Channel";
+import Footer from "./components/Footer";
+// Styles
+import "./App.css";
+
+const channels = [
+  {
+    id: "kick",
+    description: "Kick",
+    color: "palegreen",
+    isMute: false,
+    playSolo: false,
+    soundSrc: "/sounds/kick.wav"
+  },
+  {
+    id: "snare",
+    description: "Snare",
+    color: "deepskyblue",
+    isMute: false,
+    playSolo: false,
+    soundSrc: "/sounds/snare.wav"
+  },
+  {
+    id: "hihat",
+    description: "Hi-Hat",
+    color: "deeppink",
+    isMute: false,
+    playSolo: false,
+    soundSrc: "/sounds/hihat.wav"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Awesome drum machine</p>
       </header>
+      <Controls />
+      {channels.map((channel, i) => (
+        <Channel key={`chan${i}`} {...channel} />
+      ))}
+      <button>+ Add new channel</button>
+      <Footer />
     </div>
   );
 }
