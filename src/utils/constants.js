2978a1e770b791c05241b54316607343b4e2a612
elstr <eleonora.lester@gmail.com>
2020-02-05 14:03:34 -0300

    feat: Use constants

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+export const DEFAULT_CHANNELS = [
+  {
+    id: "channel-kick",
+    description: "Kick",
+    color: "palegreen",
+    mute: false,
+    solo: false,
+    soundSrc: "/sounds/kick.wav"
+  },
+  {
+    id: "channel-snare",
+    description: "Snare",
+    color: "deepskyblue",
+    mute: false,
+    solo: false,
+    soundSrc: "/sounds/snare.wav"
+  },
+  {
+    id: "channel-hihat",
+    description: "Hi-Hat",
+    color: "deeppink",
+    mute: false,
+    solo: false,
+    soundSrc: "/sounds/hihat.wav"
+  }
+];
+
+export const PROPERTIES = {
+    SOLO : "solo",
+    MUTE: "mute"
+}
