231b33d9a8ec3cbbbd0e1847e3b2c483a8e329a3
elstr <eleonora.lester@gmail.com>
2020-02-07 16:11:36 -0300

    fix: Set footer at bottom

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const FixedFooter = styled.footer`
+  position: absolute;
+  bottom: 20px;
+  right: 46.5%;
+`;
 
 const Footer = () => (
-  <footer>
-    made with
+  <FixedFooter>
+    {`made with `}
     <span aria-label="love" role="img">
       ❤️
     </span>
-  </footer>
+  </FixedFooter>
 );
 
 export default Footer;
