3663cebfb748c56854253e2517a9548581ab9593
elstr <eleonora.lester@gmail.com>
2020-02-07 15:52:17 -0300

    test: Adds APP UI test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders UI', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const title = getByText(/Awesome drum machine/i);
+  
+  const bpms = getByText(/120 bpm/i);
+
+  const kick = getByText(/Kick/i);
+  const snare = getByText(/Snare/i);
+  const hihat = getByText(/Hi-Hat/i);
+
+  const addNewChannel = getByText(/Add new channel/i);
+  
+  expect(title).toBeInTheDocument();
+  expect(bpms).toBeInTheDocument();
+  expect(kick).toBeInTheDocument();
+  expect(snare).toBeInTheDocument();
+  expect(hihat).toBeInTheDocument();
+  expect(addNewChannel).toBeInTheDocument();
+  
+  
 });
